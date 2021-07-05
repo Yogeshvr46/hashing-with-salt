81d245bb35a898adec6bbd4ae2ae5e3dbcc0d9ef
Yogesh R <53150335+Yogeshvr46@users.noreply.github.com>
2021-07-05 19:21:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,5 @@ secure = hashlib.sha256(Texthash.encode()).hexdigest()
 print("the encoded code for the text is :", secure)
 salt = uuid.uuid4().hex
 print("the salt is : ", salt)
-hashed = hashlib.sha256(Texthash.encode()).hexdigest() + ":" + salt #adding a secondary layer to a normal string
-#used : to identityfy the salt in the salted code
+hashed = hashlib.sha256(Texthash.encode()).hexdigest() + salt
 print("the salted code is : ", hashed)
